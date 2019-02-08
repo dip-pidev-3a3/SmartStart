b544f129cb6623038b87238bc0e42a0e118ad2db
mohamed amine <mohamedamine.lamari@esprit.tn>
2019-02-08 23:58:50 +0100

    good night

---
+++ src/com/smartstart/entities/Contract.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.smartstart.entities;
+
+import java.util.Date;
+
+/**
+ *
+ * @author diabl
+ */
+public class Contract {
+    private int id_contract;
+    private String payment_method;
+    private Date start_date;
+    private Date finish_date;
+    private float sum;
+    private int id_application;
+    
+    public Contract(){}
+    
+    public Contract(int id_contract,String payment_method,Date start_date,Date finish_date,float sum,int id_application){
+       this.payment_method = payment_method;
+       this.start_date = start_date;
+       this.finish_date = finish_date;
+       this.sum = sum;
+       this.id_application = id_application;
+    }
+
+    public int getId_contract() {
+        return id_contract;
+    }
+
+    public void setId_contract(int id_contract) {
+        this.id_contract = id_contract;
+    }
+
+    public String getPayment_method() {
+        return payment_method;
+    }
+
+    public void setPayment_method(String payment_method) {
+        this.payment_method = payment_method;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getFinish_date() {
+        return finish_date;
+    }
+
+    public void setFinish_date(Date finish_date) {
+        this.finish_date = finish_date;
+    }
+
+    public float getSum() {
+        return sum;
+    }
+
+    public void setSum(float sum) {
+        this.sum = sum;
+    }
+
+    public int getId_application() {
+        return id_application;
+    }
+
+    public void setId_application(int id_application) {
+        this.id_application = id_application;
+    }
+    
+    
+    
+}
--- src/com/smartstart/entities/Message.java
+++ src/com/smartstart/entities/Message.java
@@ -32,6 +32,63 @@ public class Message {
     
     }
 
+    public int getId_message() {
+        return id_message;
+    }
+
+    public void setId_message(int id_message) {
+        this.id_message = id_message;
+    }
+
+    public int getMessage_from() {
+        return message_from;
+    }
+
+    public void setMessage_from(int message_from) {
+        this.message_from = message_from;
+    }
+
+    public int getMessage_to() {
+        return message_to;
+    }
+
+    public void setMessage_to(int message_to) {
+        this.message_to = message_to;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
+
+    public Date getDate_message() {
+        return date_message;
+    }
+
+    public void setDate_message(Date date_message) {
+        this.date_message = date_message;
+    }
+
+    public int getViewed() {
+        return viewed;
+    }
+
+    public void setViewed(int viewed) {
+        this.viewed = viewed;
+    }
+    
+
     
     
     
