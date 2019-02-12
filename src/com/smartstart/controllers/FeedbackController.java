0f00bcf5e21a6c93098af7f77b404a15c3796a01
mohamed amine <mohamedamine.lamari@esprit.tn>
2019-02-12 23:07:03 +0100

    12/02/2019 23:06

--- src/com/smartstart/controllers/FeedbackController.java
+++ src/com/smartstart/controllers/FeedbackController.java
@@ -24,6 +24,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -41,7 +42,7 @@ import javafx.stage.Stage;
  *
  * @author dytcha
  */
-public class FeedbackController {
+public class FeedbackController implements Initializable {
 
     @FXML
     private AnchorPane parent;
@@ -75,6 +76,7 @@ public class FeedbackController {
     private Label Nombre_Feedback;
 
     
+    @Override
     public void initialize(URL url, ResourceBundle rb) {
         //OpportunityService s = new OpportunityService();
         //int i = s.CountOpportunities(1);
@@ -217,7 +219,7 @@ public class FeedbackController {
     @FXML
     private void displayDetails(ActionEvent event) {
         try {
-            FXMLLoader detail = new FXMLLoader(getClass().getResource("/com/smartstart/gui/DetailsContractGui.fxml"));
+            FXMLLoader detail = new FXMLLoader(getClass().getResource("/com/smartstart/gui/DetailFeedbackGui.fxml"));
             Parent root1 = (Parent) detail.load();
             Stage stage = new Stage();
             
